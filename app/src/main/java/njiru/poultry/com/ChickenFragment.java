a5414cd47c97b181543ede69fe843d9e2e921d13
Dnju <87752323+Dnju@users.noreply.github.com>
2021-09-03 17:38:37 +0300

    commit and push

--- app/src/main/java/njiru/poultry/com/ChickenFragment.java
+++ app/src/main/java/njiru/poultry/com/ChickenFragment.java
@@ -1,18 +1,71 @@
 package njiru.poultry.com;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ChickenFragment extends Fragment {
-    @Nullable
+    EditText chicken, vaccine;
+    Button save;
+    FirebaseFirestore db;
+
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_chicken, container, false);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+
+
+        db=FirebaseFirestore.getInstance();
+        chicken = findViewById(R.id.Et_chicken);
+        vaccine = findViewById(R.id.Et_vaccine);
+        save=findViewById(R.id.Chicken_Save_button);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Chicken=chicken.getText().toString();
+                String Vaccine=vaccine.getText().toString();
+                Map<String,Object>CHICKEN=new HashMap<>();
+                CHICKEN.put("Chicken",chicken);
+                CHICKEN.put("Vaccine",vaccine);
+                db.collection("CHICKEN")
+                        .add(CHICKEN)
+                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                            @Override
+                            public void onSuccess(DocumentReference documentReference) {
+                                Toast.makeText(ChickenFragment.this,"Saved Successfully",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ChickenFragment.this,"Failed",
+                                Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
     }
 }

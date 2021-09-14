bca952932ddade41d42d1d2b3ca0ff4075b8e0a2
Dnju <87752323+Dnju@users.noreply.github.com>
2021-09-14 09:59:14 +0300

    Commit and push

--- app/src/main/java/njiru/poultry/com/ChickenFormFragment.java
+++ app/src/main/java/njiru/poultry/com/ChickenFormFragment.java
@@ -2,7 +2,10 @@ package njiru.poultry.com;
 
 import static android.service.controls.ControlsProviderService.TAG;
 
+import android.annotation.SuppressLint;
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -11,13 +14,16 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -31,6 +37,7 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -41,8 +48,13 @@ public class ChickenFormFragment extends Fragment {
     private FirebaseFirestore db;
     private ImageView image3, image4;
     private FloatingActionButton floatingActionButton;
+    private TextView date;
+    private Calendar myCalendar = Calendar.getInstance();
+    private int mDate, mMonth, mYear;
+    EditText date_d;
+    ImageView cal;
 
-     @Nullable
+    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_chicken_form, container, false);
@@ -54,31 +66,50 @@ public class ChickenFormFragment extends Fragment {
         save = view.findViewById(R.id.Chicken_Save_button);
         image3 = view.findViewById(R.id.imageView3);
         image4 = view.findViewById(R.id.imageView4);
-        floatingActionButton=view.findViewById(R.id.FLOAT);
+        floatingActionButton = view.findViewById(R.id.FLOAT);
 
-         ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, chickens);
-         chicken.setAdapter(adapter);
 
-         adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, vaccines);
-         vaccine.setAdapter(adapter);
-
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+        // initiate the date picker and Edittext
+        date_d = view.findViewById(R.id.date);
+        cal = view.findViewById(R.id.datepicker);
+        // OnclickListener For the date, month and year
+        cal.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
             @Override
             public void onClick(View v) {
-                FragmentTransaction t= getFragmentManager().beginTransaction();
-                t.replace(R.id.fragment_container,new ChickenFragment());
-                t.commit();
+                final Calendar cal = Calendar.getInstance();
+                mDate = cal.get(Calendar.DATE);
+                mMonth = cal.get(Calendar.MONTH);
+                mYear = cal.get(Calendar.YEAR);
 
-
-
-
-
-
+                DatePickerDialog datePickerDialog = new DatePickerDialog(getContext(), android.R.style.Theme_DeviceDefault_Dialog,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int month, int date) {
+                                date_d.setText(date + "-" + month + "-" + year);
+                            }
+                        }, mYear, mMonth, mDate);
+                datePickerDialog.show();
             }
         });
 
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, chickens);
+        chicken.setAdapter(adapter);
 
+        adapter = new ArrayAdapter<String>(getContext(), android.R.layout.simple_dropdown_item_1line, vaccines);
+        vaccine.setAdapter(adapter);
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction t = getFragmentManager().beginTransaction();
+                t.replace(R.id.fragment_container, new ChickenFragment());
+                t.commit();
+
+
+            }
+        });
 
 
         //Chicken dropdown
@@ -99,7 +130,7 @@ public class ChickenFormFragment extends Fragment {
             }
         });
 
-         // Acquire or get chicken and vaccine entered data.
+        // Acquire or get chicken and vaccine entered data.
         save.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -135,6 +166,7 @@ public class ChickenFormFragment extends Fragment {
         return view;
 
     }
+
     // Chicken and vaccine values to be displayed on click image.
     private static final String[] chickens = new String[]{"Broilers", "Layers"};
     private static final String[] vaccines = new String[]{"Mareks Disease Vaccine(HVT)", "Ranikhet Disease Vaccine",

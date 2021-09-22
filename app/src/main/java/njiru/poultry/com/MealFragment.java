57ee18d7d48ca1b3c76923c07053683c8273d416
Dnju <87752323+Dnju@users.noreply.github.com>
2021-09-22 11:34:13 +0300

    commit

--- app/src/main/java/njiru/poultry/com/MealFragment.java
+++ app/src/main/java/njiru/poultry/com/MealFragment.java
@@ -1,93 +1,115 @@
 package njiru.poultry.com;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter;
 import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MealFragment extends Fragment {
-    private TextView meal_name, meal_quantity;
-    private FirebaseFirestore fStore;
-    private RecyclerView mFirestorelist;
-    private FirestoreRecyclerAdapter adapter;
 
+    private RecyclerView recyclerView;
+    private FloatingActionButton floatingActionButton;
+    ArrayList<Meals> mealsArrayList;
+    MealsAdapter mealsAdapter;
+    FirebaseFirestore db;
+    ProgressDialog progressDialog;
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_meal, container, false);
-    mFirestorelist=view.findViewById(R.id.Recyclerview_meal);
-    fStore=FirebaseFirestore.getInstance();
-    mFirestorelist=view.findViewById(R.id.Recyclerview_chicken);
 
 
-         //Query
-        Query query=fStore.collection("MEALS");
+     progressDialog=new ProgressDialog(getContext());
+        progressDialog.setMessage("Fetching data.....");
+        progressDialog.show();
 
-        // RecyclerOptions
-        FirestoreRecyclerOptions<MealModel>options=new FirestoreRecyclerOptions.Builder<MealModel>()
-                .setQuery(query,MealModel.class)
-                .build();
 
-        //Firestore RecyclerAdapter
-      adapter= new FirestoreRecyclerAdapter<MealModel, MealViewHolder>(options) {
-            @NonNull
+        recyclerView = view.findViewById(R.id.Recyclerview_meal);
+        floatingActionButton = view.findViewById(R.id.Fab_meal);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+
+        db = FirebaseFirestore.getInstance();
+
+        mealsArrayList = new ArrayList<Meals>();
+        mealsAdapter = new MealsAdapter(getContext(), mealsArrayList);
+        recyclerView.setAdapter(mealsAdapter);
+
+        EventChangeListener();
+
+
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public MealViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                View view1=LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item_meal, parent, false);
-                return new MealViewHolder(view1);
+            public void onClick(View v) {
+                FragmentTransaction mm = getFragmentManager().beginTransaction();
+                mm.replace(R.id.fragment_container, new MealFormFragment());
+                mm.commit();
             }
+        });
 
-            @Override
-            protected void onBindViewHolder(@NonNull MealViewHolder holder, int position, @NonNull MealModel model) {
-                holder.list_Meal.setText(model.getMeal());
-                holder.list_quantity.setText(model.getQuantity());
-            }
-        };
-
-
-     mFirestorelist.setHasFixedSize(true);
-     mFirestorelist.setLayoutManager(new LinearLayoutManager(getContext()));
-     mFirestorelist.setAdapter(adapter);
-
-
-
-
-//ViewHolder
         return view;
     }
 
-    private class MealViewHolder extends RecyclerView.ViewHolder{
-        private TextView list_Meal;
-        private TextView list_quantity;
-        public MealViewHolder(@NonNull View itemView) {
-            super(itemView);
-            list_Meal=itemView.findViewById(R.id.list_meal);
-            list_quantity=itemView.findViewById(R.id.list_quantity);
-        }
+    private void EventChangeListener() {
+        db.collection("MEALS").orderBy("Meals", Query.Direction.ASCENDING)
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot value, @Nullable FirebaseFirestoreException error) {
+                        if (error != null) {
 
-    }
+                            if (progressDialog.isShowing())
+                                progressDialog.dismiss();
 
-    @Override
-    public void onStart() {
-        super.onStart();
-        adapter.startListening();
-    }
+                            Log.e("Firestore error", error.getMessage());
+                            return;
 
-    @Override
-    public void onStop() {
-        super.onStop();
-        adapter.stopListening();
+                        }
+
+                        for (DocumentChange dc : value.getDocumentChanges()) {
+
+                            if (dc.getType() == DocumentChange.Type.ADDED) {
+
+                                mealsArrayList.add(dc.getDocument().toObject(Meals.class));
+
+                            }
+
+                            mealsAdapter.notifyDataSetChanged();
+
+                            if (progressDialog.isShowing())
+                                progressDialog.dismiss();
+                        }
+
+                    }
+                });
     }
 }
 
---
+++ app/src/main/java/njiru/poultry/com/MealsAdapter.java
@@ -0,0 +1,57 @@
+package njiru.poultry.com;
+
+import android.app.DownloadManager;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+public class MealsAdapter extends RecyclerView.Adapter<MealsAdapter.MealsViewHolder> {
+    Context context;
+    ArrayList<Meals> MealsArrayList;
+
+    public MealsAdapter(Context context, ArrayList<Meals> mealsArrayList) {
+        this.context = context;
+        MealsArrayList = mealsArrayList;
+    }
+
+    @NonNull
+    @Override
+    public MealsAdapter.MealsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v= LayoutInflater.from(context).inflate(R.layout.list_item_meal,parent,false);
+        return  new MealsViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MealsAdapter.MealsViewHolder holder, int position) {
+        Meals meals= MealsArrayList.get(position);
+
+        holder.Meals.setText(meals.Meals);
+        holder.Quantity.setText(meals.Quantity);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return MealsArrayList.size();
+
+    }
+
+    public static class MealsViewHolder extends RecyclerView.ViewHolder{
+     TextView Meals, Quantity;
+
+        public MealsViewHolder(@NonNull View itemView) {
+            super(itemView);
+            Meals=itemView.findViewById(R.id.list_meal);
+            Quantity=itemView.findViewById(R.id.list_quantity);
+
+        }
+    }
+}
